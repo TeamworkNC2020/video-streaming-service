75b6eb67009b76160626c3cf1b915c9a050fc4bc
NartovEvgenii <evgeniynartov36rus@gmail.com>
2020-12-06 18:36:57 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/video/streaming/service/Application.java
@@ -0,0 +1,17 @@
+package com.mycompany.video.streaming.service;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+            System.out.println("Hello world");
+                        
+	}
+
+}
---
+++ src/main/java/com/mycompany/video/streaming/service/ClassExample.java
@@ -0,0 +1,17 @@
+package com.mycompany.video.streaming.service;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ClassExample {
+
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
+}
