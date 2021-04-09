74dc63aac652083d93a08511173e8338584a05bc
weldnor <kolesov.anton.s@gmail.com>
2021-04-09 19:50:40 +0300

    refactor app and pom

--- src/main/java/com/moviesandchill/video/streaming/service/Application.java
+++ src/main/java/com/moviesandchill/video/streaming/service/Application.java
@@ -2,21 +2,9 @@ package com.moviesandchill.video.streaming.service;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-@EntityScan(basePackages = {"com.moviesandchill.video.streaming.service.domain" })
-@EnableJpaRepositories(basePackages = {"com.moviesandchill.video.streaming.service.repository"})
-
 public class Application {
 
 	public static void main(String[] args) {
